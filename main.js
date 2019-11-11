6ee1873b7c70e150d2a2ca9903d0cf2c315f54d3
Arvin <Arvin@Arvins-MacBook-Pro.local>
2019-11-11 23:46:58 +0100

    Make copies from exisiting repo
     - Also make some chnages

---
+++ main.js
@@ -0,0 +1,23 @@
+const util = require("./myUtils.js");
+const reader = require ("readLine-sync");
+
+
+const firstPositive = reader.questionInt("Enter the first positive number: ");
+const secondPositive = reader.questionInt("Enter the second positive number: ");
+
+let arr = new Array();
+
+for (let i = firstPositive ; i <= secondPositive; i++){
+    if(util.isPrime(i)){
+        
+        arr.push(i);
+        
+    }
+}
+util.printArrayItems(arr);
+     
+
+
+
+
+
---
+++ myUtils.js
@@ -0,0 +1,24 @@
+module.exports = {
+	isPrime: function(n) {
+		if(n <=1) {
+			return false;
+		}
+
+		let div = 2;
+
+		while(div <= Math.sqrt(n)) {
+			if(n % div === 0) {
+				return false;
+			}
+
+			div++;
+		}
+
+		return true;
+	},
+	printArrayItems: function(array) {
+for (let i = 0; i < array.length; i++)
+console.log(array[i]);
+	}
+
+}
